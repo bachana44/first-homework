980ec11737dc63093db90e6d59583760713b53fb
bachana <bachanayor11@gmail.com>
2025-10-06 14:42:04 +0400

    six-homework

---
+++ six-homework/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace six_homework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("chaweret teqsti: ");
+            string userText = Console.ReadLine();
+            string firstLetter = userText[0].ToString().ToUpper();
+            
+
+            userText = userText.Replace(userText[0].ToString(),firstLetter);
+            Console.WriteLine(userText);
+            
+        }
+    }
+}
